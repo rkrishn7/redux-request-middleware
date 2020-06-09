a41599aac9c12ad625d3fbe01b08f79c8db56cdf
Rohan Krishnaswamy <rohan@rohankrishnaswamy.com>
2020-06-09 09:26:07 -0700

    Exported connected component via ReactRedux

--- example/static/modules/component.js
+++ example/static/modules/component.js
@@ -1,26 +1,67 @@
-export default class Main extends React.Component {
+import {
+    setRefreshToken,
+    setAccessToken
+} from "./store.js";
+
+class Main extends React.Component {
+
+    static #ORIGIN = "http://localhost:3000/api/";
 
     constructor(props) {
         super(props);
+
         this.state = {
-            response: null
+            response: null,
+            error: "None"
         };
 
         this.makeRequest = this.makeRequest.bind(this);
     }
 
     makeRequest() {
-        this.setState({
-            response: "hello"
-        });
+        console.log('hello');
+        fetch(Main.#ORIGIN + 'protected/', {
+            headers: {
+                "Authorization" : "Bearer sdsdf"
+            }
+        }).then((response) => {
+            return response.json();
+        }).then((json) => {
+            this.setState((state) => {
+                return (json.error) ? {
+                    ...state,
+                    error: json.error
+                } : {
+                    ...state,
+                    response: "OK"
+                };
+            });
+        })
+    }
+
+    getAccessToken() {
+
     }
 
     render() {
         return (
             React.createElement("div", null, React.createElement("button", {
                 onClick: this.makeRequest
-              }, "send request to protected route"), 
-              React.createElement("div", null, "response: ", this.state.response))
+              }, "send request to protected route"), React.createElement("div", null, "Response: ", this.state.response), 
+              React.createElement("button", {
+                onClick: () => {}
+              }, "Get access token"), 
+              React.createElement("div", null, "Error: ", this.state.error),
+              React.createElement("div", null, "Access Token: ", this.props.access))
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        access: state.access,
+        refresh: state.refresh,
+    };
+}
+
+export default ReactRedux.connect(mapStateToProps)(Main);
